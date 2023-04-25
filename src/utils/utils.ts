a484031eebb18423307a9a9fb7d4181281d06fee
afiatamanna06 <afiatamanna06@gmail.com>
2023-04-25 14:01:46 +0600

    Add blog filter function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,3 +14,41 @@ export function formatDate(date: string) {
       timeZone: "UTC",
     })
 }
+
+export function formatBlogPosts(posts: any, {
+  filterOutDrafts = true,
+  filterOutFuturePosts = true,
+  sortByDate = true,
+  limit = undefined,
+} = {}) {
+
+  const filteredPosts = posts.reduce((acc: any, post: any) => {
+    const { date, draft } = post.frontmatter;
+    
+    if (filterOutDrafts && draft) return acc;
+
+    
+    if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+    post.frontmatter.date = new Date(date)
+    
+    acc.push(post)
+
+    return acc;
+  }, [])
+
+  
+  console.log(filteredPosts)
+  if (sortByDate) {
+    filteredPosts.sort((a: any, b: any) => b.frontmatter.date - a.frontmatter.date)
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+  return filteredPosts;
+
+}
